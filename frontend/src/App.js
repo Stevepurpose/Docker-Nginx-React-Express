2229c007dfe15fe1bbc9c7f5b20bc7e9672f9b60
stevepurpose <stevenpurpose@gmail.com>
2024-07-25 05:02:03 +0100

    added error handling in App.js and changed environmental variable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,29 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
- let[myarr, setMyArr]=useState([])
+  let [myarr, setMyArr] = useState([]);
 
-useEffect(()=>{
-   
-      fetch(process.env.REACT_APP_API_URL).then(res=>{
-       return res.json() 
-     })
-     .then(myarr=>
-      
-  setMyArr(myarr)
-     )
-  
-},[])
-
+  useEffect(() => {
+    fetch(process.env.REACT_APP_APIURL)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then(data => setMyArr(data))
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
     <div className="App">
-      <h1>Some Even Numbers Greater than 6  and Less than 20 When Multilied By 2</h1>
+      <h1>Some Even Numbers Greater than 6 and Less than 20 When Multiplied By 2</h1>
       <div>
-      {myarr.map((element, index) =>(
-        <p key={index}>{element * 2}</p>
-      ))}
-      
+        {myarr.map((element, index) => (
+          <p key={index}>{element * 2}</p>
+        ))}
       </div>
     </div>
   );
